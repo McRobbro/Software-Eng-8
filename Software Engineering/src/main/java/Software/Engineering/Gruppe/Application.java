a2008347513b459a9d365d5871bcc229c0cc0230
Robby <rdahlman32@gmail.com>
2021-10-04 21:24:14 +0200

    Vue Files added and store-overview.vue works.

--- Software Engineering/src/main/java/Software/Engineering/Gruppe/Application.java
+++ Software Engineering/src/main/java/Software/Engineering/Gruppe/Application.java
@@ -6,33 +6,50 @@ import Software.Engineering.Gruppe.Repository.StoreRepository;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
+import io.javalin.http.staticfiles.Location;
+import io.javalin.plugin.rendering.vue.*;
 import org.jetbrains.annotations.NotNull;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.function.Consumer;
 
 public class Application {
 
     public static void main(String[] args) {
-
+        // Init Connection to database
         SQLDatabase SQLdatabase = new SQLDatabase(
                 "jdbc:mysql://34.88.134.36:3306/soe_group_8",
             "root",
             "group8ftw"
         );
 
+        // init javalin web service
+        Javalin app = Javalin.create(config -> {
+            config.enableWebjars();
+        }).start(7000);
+        JavalinVue.rootDirectory(c -> c.classpathPath("/vue"));
+        JavalinVue.vueVersion(c -> c.vue3("app"));
+        // Init Vue Files
+        app.get("/stores", new VueComponent("store-overview"));
+        app.get("/stores/{store-id}", new VueComponent("store-detail"));
+        app.error(404, ctx -> {
+            ctx.result("Generic 404 message");
+        });
+
         // Init repos
         StoreRepository storeRepository = new StoreRepository(SQLdatabase);
-
         // Init controllers
         StoresController storesController = new StoresController(storeRepository);
 
-
-        Javalin app = Javalin.create().start(7000);
-
+        //redirect to homepage
+        app.before("/", ctx -> ctx.redirect("/home"));
         // Register routes and handlers
-        app.get("/", ctx -> {
+        app.get("/home", ctx -> {
             ctx.result("Platform home page");
         });
 
-        app.get("/stores", storesController::getAllStores);
+        app.get("/api/stores", storesController::getAllStores);
 
     }
 }
