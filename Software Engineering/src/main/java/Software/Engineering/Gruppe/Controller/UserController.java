85d41abc7153128d5e7afdeaff84f4b5f5b84927
Ingvild Louise Eide <ingvild.l.eide@hiof.no>
2021-10-21 17:09:46 +0200

    Test branch userController

---
+++ Software Engineering/src/main/java/Software/Engineering/Gruppe/Controller/UserController.java
@@ -0,0 +1,11 @@
+package Software.Engineering.Gruppe.Controller;
+
+import Software.Engineering.Gruppe.Model.Product;
+import Software.Engineering.Gruppe.Repository.ProductRepository;
+import io.javalin.http.Context;
+import java.util.List;
+
+
+// Test
+public class UserController {
+}
