217ba636d51888f4c922691222017395daa827ea
Fleischerboy <78142977+Fleischerboy@users.noreply.github.com>
2021-10-26 19:44:53 +0200

    added Order model and Orderline model

---
+++ Software Engineering/src/main/java/Software/Engineering/Gruppe/Model/Order.java
@@ -0,0 +1,52 @@
+package Software.Engineering.Gruppe.Model;
+
+import java.time.LocalDate;
+
+public class Order {
+
+    private int orderId;
+    private LocalDate orderDate;
+    private User userId;
+    private Store storeId;
+
+    public Order(int orderId, LocalDate orderDate, User userId, Store storeId) {
+        this.orderId = orderId;
+        this.orderDate = orderDate;
+        this.userId = userId;
+        this.storeId = storeId;
+    }
+
+
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public User getUserId() {
+        return userId;
+    }
+
+    public void setUserId(User userId) {
+        this.userId = userId;
+    }
+
+    public Store getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(Store storeId) {
+        this.storeId = storeId;
+    }
+}
