0dec9d3f69f100437f4cc8ec9f2b68a57ad1263e
Robby <rdahlman32@gmail.com>
2021-10-26 15:21:58 +0200

    Massive overhaul of Product and its features

--- Software Engineering/src/main/java/Software/Engineering/Gruppe/Controller/ProductController.java
+++ Software Engineering/src/main/java/Software/Engineering/Gruppe/Controller/ProductController.java
@@ -4,6 +4,8 @@ import Software.Engineering.Gruppe.Model.Product;
 import Software.Engineering.Gruppe.Repository.ProductRepository;
 
 import io.javalin.http.Context;
+
+import java.sql.SQLException;
 import java.util.List;
 
 
@@ -19,10 +21,23 @@ public class ProductController {
         context.json(allStores);
     }
 
+    public void getProductsFromStore(Context context) {
+        String storeSlug = context.pathParam("{storeSlug}");
+        List<Product> getProducts = productRepository.getProductsFromStore(storeSlug);
+        context.json(getProducts);
+    }
+
     public void getSpecificProduct(Context context) {
-        String slug = context.pathParam("{slug}");
-        Product getProductBySlug = productRepository.getSpecificProductBySlug(slug);
-        context.json(getProductBySlug);
+        String storeSlug = context.pathParam("{storeSlug}");
+        String prodSlug = context.pathParam("{prodSlug}");
+
+        Product product = productRepository.getSpecificProduct(storeSlug, prodSlug);
+        context.json(product);
+    }
+
+    public void deleteProduct(Context context) {
+        String storeSlug = context.pathParam("{storeSlug}");
+        context.redirect("/stores/" + storeSlug);
     }
 
 }
