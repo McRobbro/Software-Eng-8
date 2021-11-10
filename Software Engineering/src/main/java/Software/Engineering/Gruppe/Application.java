1abd3f2e6668835fa7a881f81dc5ea3eb562df2b
Robby <rdahlman32@gmail.com>
2021-11-10 18:27:19 +0100

    Working on Roles.

--- Software Engineering/src/main/java/Software/Engineering/Gruppe/Application.java
+++ Software Engineering/src/main/java/Software/Engineering/Gruppe/Application.java
@@ -7,21 +7,45 @@ import Software.Engineering.Gruppe.Repository.ProductRepository;
 import Software.Engineering.Gruppe.Repository.StoreRepository;
 
 import io.javalin.Javalin;
+import io.javalin.core.security.RouteRole;
+import io.javalin.http.Context;
 import io.javalin.plugin.rendering.vue.*;
 
 public class Application {
 
 
     public static void main(String[] args) {
+        /*
+        enum Role implements RouteRole {
+            ANYONE, LOGGED_IN;
+        }
+
+         */
 
         String userDir = System.getProperty("user.dir");
         String databaseDir = "\\db\\group8dbftw.db";
         // init Connection to sqlite database
         SqliteDatabase sqliteDatabase = new SqliteDatabase("jdbc:sqlite:" + userDir + databaseDir);
+        // Init repos
+        StoreRepository storeRepository = new StoreRepository(sqliteDatabase);
+        ProductRepository productRepository = new ProductRepository(sqliteDatabase, storeRepository);
+        // Init controllers
+        StoresController storesController = new StoresController(storeRepository);
+        ProductController productController = new ProductController(productRepository);
 
         // init javalin web service
         Javalin app = Javalin.create(config -> {
             config.enableWebjars();
+            /*
+            config.accessManager((handler, ctx, permittedRoles) -> {
+                if (permittedRoles.contains(getUserRole(ctx))) {
+                    handler.handle(ctx);
+                } else {
+                    ctx.status(401);
+                }
+            });
+
+             */
         }).start(7777);
         JavalinVue.rootDirectory(c -> c.classpathPath("/vue"));
         JavalinVue.vueVersion(c -> c.vue3("app"));
@@ -36,22 +60,15 @@ public class Application {
         // Init Vue Files
         app.get("/stores", new VueComponent("store-overview"));
         app.get("/stores/{storeSlug}", new VueComponent("store-detail"));
-        app.get("/stores/{storeSlug}/products/{prodSlug}", new VueComponent("product-detail"));
+        app.get("/stores/{storeSlug}/{prodSlug}", new VueComponent("product-detail"));
         app.get("/login", new VueComponent("login-page"));
         app.error(404, ctx -> {
             ctx.result("Generic 404 message");
         });
 
-        // Init repos
-        StoreRepository storeRepository = new StoreRepository(sqliteDatabase);
-        ProductRepository productRepository = new ProductRepository(sqliteDatabase, storeRepository);
-        // Init controllers
-        StoresController storesController = new StoresController(storeRepository);
-        ProductController productController = new ProductController(productRepository);
-
         app.get("/api/stores", storesController::getAllStores);
         app.get("/api/stores/{storeSlug}", productController::getProductsFromStore);
-        app.get("/api/stores/{storeSlug}/products/{prodSlug}", productController::getSpecificProduct);
+        app.get("/api/stores/{storeSlug}/{prodSlug}", productController::getSpecificProduct);
 
         app.get("/api/products", productController::getAllProducts);
 
